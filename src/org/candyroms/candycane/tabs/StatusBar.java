fd239c36a03c7714660e9d2c412a3ce4e03ed3e4
gimmeitorilltell <gimmeitorilltell@yahoo.com>
2017-07-13 23:54:13 -0400

    (2of2) Cleanup old statusbar weather to make room for OmniJaws to work properly. And remove some old statusbar code until we can update it.
    Change-Id: Ibebfdaae24302b6da12759623833363d7c77bf2d

--- src/org/candyroms/candycane/tabs/StatusBar.java
+++ src/org/candyroms/candycane/tabs/StatusBar.java
@@ -16,36 +16,34 @@
 
 package org.candyroms.candycane.tabs;
 
-import android.content.Context;
 import android.content.ContentResolver;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
-import android.preference.ListPreference;
-import android.preference.SwitchPreference;
-import android.preference.Preference;
-import android.preference.PreferenceCategory;
-import android.preference.PreferenceScreen;
-import android.preference.Preference.OnPreferenceChangeListener;
 import android.provider.Settings;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v14.preference.PreferenceFragment;
 
+import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.settings.Utils;
+import com.android.internal.util.candy.CandyUtils;
+
 
 public class StatusBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
-    private static final String TAG = "StatusBar";
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.statusbar);
+    private static final String PREF_STATUS_BAR_WEATHER = "status_bar_weather";
 
-        ContentResolver resolver = getActivity().getContentResolver();
-    }
+    private SwitchPreference mEnableNC;
+    private ListPreference mStatusBarWeather;
 
     @Override
     protected int getMetricsCategory() {
@@ -53,19 +51,49 @@ public class StatusBar extends SettingsPreferenceFragment implements
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.statusbar);
+
+        final PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+       // Status bar weather
+       mStatusBarWeather = (ListPreference) findPreference(PREF_STATUS_BAR_WEATHER);
+       int temperatureShow = Settings.System.getIntForUser(resolver,
+               Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP, 0,
+               UserHandle.USER_CURRENT);
+       mStatusBarWeather.setValue(String.valueOf(temperatureShow));
+       if (temperatureShow == 0) {
+           mStatusBarWeather.setSummary(R.string.statusbar_weather_summary);
+       } else {
+           mStatusBarWeather.setSummary(mStatusBarWeather.getEntry());
+       }
+          mStatusBarWeather.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
     public void onResume() {
         super.onResume();
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+		ContentResolver resolver = getActivity().getContentResolver();
+        if  (preference == mStatusBarWeather) {
+            int temperatureShow = Integer.valueOf((String) newValue);
+            int index = mStatusBarWeather.findIndexOfValue((String) newValue);
+            Settings.System.putIntForUser(
+                   resolver, Settings.System.STATUS_BAR_SHOW_WEATHER_TEMP,
+                   temperatureShow, UserHandle.USER_CURRENT);
+            if (temperatureShow == 0) {
+                mStatusBarWeather.setSummary(R.string.statusbar_weather_summary);
+            } else {
+                mStatusBarWeather.setSummary(
+                mStatusBarWeather.getEntries()[index]);
+            }
+            return true;
+        }
+        return false;
     }
-
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        final String key = preference.getKey();
-        return true;
-    }
-
 }
-
